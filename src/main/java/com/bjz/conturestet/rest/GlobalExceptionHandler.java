28e46fa36fa83d9af4759d75a3f2714fb30ff9c9
alex-bajzat <alex.bajzat@gmail.com>
2018-10-01 01:02:38 +0300

    Added exception loggin to the global exception handler

--- src/main/java/com/bjz/conturestet/rest/GlobalExceptionHandler.java
+++ src/main/java/com/bjz/conturestet/rest/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.bjz.conturestet.rest;
 
 import com.bjz.conturestet.rest.response.APIError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Order(value = 9999)
 public class GlobalExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<APIError> handleAnyException(Exception e) {
+        LOGGER.error(e.getMessage(), e);
         return new ResponseEntity<>(new APIError()
                 .setCode(APIError.ErrorCode.UNKNOWN)
-                .setMessage("Operation not supported"), HttpStatus.INTERNAL_SERVER_ERROR);
+                .setMessage("Something went wrong"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
